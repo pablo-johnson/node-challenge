3e7db839475a7f8fe5a4e21d895a4f24e7c55211
Pablo Johnson <pablo.88j@gmail.com>
2023-07-15 15:36:04 +0200

    Dotenv install
    Listening port variable definition

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
 
 async function bootstrap() {
+  config()
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.LISTENING_PORT);
 }
 bootstrap();
